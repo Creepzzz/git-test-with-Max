f496407e626587411ce69217fe7f70a5a6605710
Matilda Qvick <60852111+Creepzzz@users.noreply.github.com>
2021-05-23 18:37:43 +0200

    Update Login.test.js

--- login/Login.test.js
+++ login/Login.test.js
@@ -5,6 +5,7 @@ async function btn(){
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("http://localhost:3000/#login");
     await driver.findElement(By.className("logInBtn")).click();
+    await driver.sleep(100);
 }
 
 btn();
